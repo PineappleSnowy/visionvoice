ed6ab83722cbca11438e738ee909abc260ade029
Dawn <1470412156@qq.com>
2024-03-02 10:30:04 +0800

    add some illustration

--- web_camera.py
+++ web_camera.py
@@ -18,21 +18,23 @@ def save_audio(text):
 
 def final_realize():
     app = Flask(__name__)
+    # CORS解决跨域访问问题
     CORS(app)
     socketio = SocketIO(app)
-    direction = multiprocessing.Value('i', 0)
-    judge = multiprocessing.Value('b', True)
-    judge2 = multiprocessing.Value('b', True)
-    shoot = multiprocessing.Value('b', False)
-    shoot2 = multiprocessing.Value('b', False)
-    shoot3 = multiprocessing.Value('b', False)
+    direction = multiprocessing.Value('i', 0)  # 人脸位置标签
+    judge = multiprocessing.Value('b', True)  # 判断是否播放欢饮音频
+    judge2 = multiprocessing.Value('b', True)  # 播放欢迎音频的音频选择标签
+    shoot = multiprocessing.Value('b', False)  # 拍照
+    shoot2 = multiprocessing.Value('b', False)  # 播放拍照成功音频
+    shoot3 = multiprocessing.Value('b', False)  # 保持拍照后画面静止
     img = None
     image = None
-    temp = time.time()
-    temp2 = time.time()
+    temp = time.time()  # 方向提示音频播放时长
+    temp2 = time.time()  # 页面加载等待及欢迎音频播放时长
 
     @app.route('/')
     def index():
+        # 初始化参数信息
         nonlocal temp2
         temp2 = time.time()
         judge.value = True
@@ -56,36 +58,45 @@ def final_realize():
         # 将Base64编码的图像数据解码为numpy数组
         encoded_data = frame.split(',')[1]
         np_arr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
+        # 开始时，视频帧可能过段时间才向后端传递，此时会产生异常，故用try
         try:
             # 将numpy数组转换为OpenCV的图像对象
             img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
             img = cv2.flip(img, 1)
             # 等待开始音频播放完毕
             if time.time() - temp2 < 8:
+                # 播放欢迎音频时框出人脸并反馈方位
                 height_, width_ = img.shape[:2]
                 detect_face(img, direction, width_, height_)
                 image = cv2.imencode('.jpg', img)[1].tobytes()
                 return "image"
             if shoot3.value:
+                # 保持画面静止
                 return ""
             if shoot2.value:
+                # 播放拍照成功
                 shoot3.value = True
                 return "announce"
             if not shoot.value:
+                # 常态下框出人脸并反馈方位
                 height_, width_ = img.shape[:2]
                 detect_face(img, direction, width_, height_)
             else:
+                # 此时shoot==True，拍照，不画人脸框
                 shoot2.value = True
             image = cv2.imencode('.jpg', img)[1].tobytes()
+            # 1.5秒等待方向音频播放完毕
             if direction.value == 0 or (time.time() - temp < 1.5):
                 return "image"
             else:
+                # 更新时间
                 temp = time.time()
                 return "announce"
         except Exception:
             return ""
 
     def gen():
+        # 处理image仍为None的异常
         try:
             return (b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
@@ -94,12 +105,13 @@ def final_realize():
 
     @app.route('/video_feed')
     def video_feed():
+        # 访问gen()函数，返回图片字节信息
         return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
     @app.route('/audio')
     def audio():
         filename = "output.wav"  # 音频文件的路径
-        if judge2.value:
+        if judge2.value:  # 判断是否播放欢迎音频
             judge2.value = False
             save_audio("欢迎使用视界之声智慧拍照")
         elif shoot3.value:
@@ -121,12 +133,16 @@ def final_realize():
     # 处理WebSocket连接和消息
     @socketio.on('message')
     def handle_message(message):
+        # message内容包括后端路由文本返回值以及前端发来的文本消息
         print(message)
         nonlocal temp
         if message == "shoot":
+            # 前端发来拍照或再拍一张的指令，修改一系列参量
             shoot.value = not shoot.value
+            # 以下两个参数修改为再拍一张服务，对拍照指令不影响
             shoot2.value = False
             shoot3.value = False
+            # 给拍照成功的语音播报提供更多时间
             temp = time.time() + 1.5
         # 向前端发送接收到的文本消息
         emit('message', message)
--- web_img_analyse.py
+++ web_img_analyse.py
@@ -10,19 +10,24 @@ import ctypes
 from p3_ImageUnderstanding import spark_chat, image_understanding
 import pyttsx3
 
+# 初始化时间标志参量
 temp = time.time()
 restart = False
 model_process = multiprocessing.Process()
 model_process2 = multiprocessing.Process()
+# 添加到环境识别的最大图片数量
 max_img_num = 3
 
 
 def analyse_img_process(flag_, img_data_list_, scene_value_, answer_value_):
     while True:
+        # 图片数量达到最大，开始环境识别最终阶段
         if flag_.value > max_img_num:
             break
     spark_chat(img_data_list_, scene_value_, answer_value_)
+    # 保存回答音频
     save_audio(answer_value_.value)
+    # 更新flag状态标志
     flag_.value = max_img_num + 2
 
 
@@ -35,26 +40,29 @@ def save_audio(text):
 def final_realize():
     global model_process, model_process2
     app = Flask(__name__)
+    # CORS解决跨域访问问题
     CORS(app)
     socketio = SocketIO(app)
-    flag = multiprocessing.Value('i', 0)
-    img_data_list = multiprocessing.Manager().list([])
-    scene_value = multiprocessing.Manager().Value(ctypes.c_char_p, "indoor")
-    answer_value = multiprocessing.Manager().Value(ctypes.c_char_p, "")
-
+    flag = multiprocessing.Value('i', 0)  # 状态标志
+    img_data_list = multiprocessing.Manager().list([])  # 图片信息列表
+    scene_value = multiprocessing.Manager().Value(ctypes.c_char_p, "indoor")  # 场景分类
+    answer_value = multiprocessing.Manager().Value(ctypes.c_char_p, "")  # 回答
+    # 这里定义了两个环境识别的进程，用于处理用户重新开始时的进程中止缓慢问题
     model_process = multiprocessing.Process(target=analyse_img_process,
                                             args=(flag, img_data_list, scene_value, answer_value))
     model_process2 = multiprocessing.Process(target=analyse_img_process,
                                              args=(flag, img_data_list, scene_value, answer_value))
 
-    def temp_process():
-        model_process.start()
-
+    # 初始化各种参数并刷新进程
     def init():
         global restart, temp, model_process, model_process2
+        # 更新restart标志，避免重复初始化
         restart = False
+        # 更新temp时间标志，开始播报欢迎语音
         temp = time.time()
+        # 初始化flag，避免函数访问正在中止的进程
         flag.value = 0
+        # 两个进程交替开始与中止
         if model_process.is_alive():
             model_process.terminate()
             model_process2 = multiprocessing.Process(target=analyse_img_process,
@@ -69,12 +77,14 @@ def final_realize():
             model_process = multiprocessing.Process(target=analyse_img_process,
                                                     args=(flag, img_data_list, scene_value, answer_value))
             model_process.start()
+        # 再次初始化flag，避免进程中止缓慢篡改flag的value值
         flag.value = 0
         answer_value.value = ""
         img_data_list[:] = []
 
     @app.route('/')
     def route_():
+        # 每次访问根网页时都进行一次初始化
         init()
         return render_template("index.html")
 
@@ -87,32 +97,38 @@ def final_realize():
     @app.route('/process_frame', methods=['GET', 'POST'])
     def process_frame():
         if restart:
-            init()
+            init()  # 重新开始
+        # 2秒钟等待网页加载，然后播放欢迎音频
         if time.time() - temp > 2:
             if flag.value == 0:
+                # 这里不使用+=是为了避免多次请求同时访问flag内存值时，其值不正常
                 flag.value = 1
                 save_audio(
                     "欢迎使用视界之声环境识别。听到录像开始后请缓慢地转动镜头，总时长15秒，"
                     "我将根据镜头画面识别当前环境。当画面重复时我会自动停止。录像开始")
+                # output会发送给前端，使前端向后端请求音频
                 return "output"
+        # 18秒等待欢饮音频播放完毕
         if not time.time() - temp > 18:
-            return "视界之声的回答：\n  "
+            return ""
         else:
             frame_data = request.get_json()
             frame = frame_data['frame']
             # 将Base64编码的图像数据解码为numpy数组
             encoded_data = frame.split(',')[1]
             np_arr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
+            # 对图片进行环境识别并更新flag状态标志值
             if flag.value <= max_img_num:
                 try:
                     # 将numpy数组转换为OpenCV的图像对象
                     img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
                     img_data_list.extend(image_understanding(img))
+                    # 为避免多次访问使flag值不正常，使用min函数
                     flag.value = min(flag.value + 1, max_img_num + 1)
                 except Exception:
                     pass
                 return "视界之声的回答：\n  "
-            # 正在进行最终环境识别
+            # 正在进行最终环境识别，显示实时结果
             elif flag.value == max_img_num + 1:
                 return "视界之声的回答：\n  " + answer_value.value
             # 最终环境识别结束，显示最终结果
@@ -125,7 +141,9 @@ def final_realize():
                 return "output"
             else:
                 return "视界之声的回答：\n  " + answer_value.value
+        # 异常时可能会运行到这里
         print("erro********************")
+        # 避免"no valid return"错误
         return ""
 
     # 处理WebSocket连接和消息
@@ -133,12 +151,12 @@ def final_realize():
     def handle_message(message):
         global restart
         if message == 'Restarted!':
+            # 重新开始
             print("restarted###########")
             restart = True
         # 向前端发送接收到的文本消息
         emit('message', message)
 
-    temp_process()
     socketio.run(app, host='0.0.0.0', port=5000, log_output=True, use_reloader=True, allow_unsafe_werkzeug=True)
 
 
